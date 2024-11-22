16451c3e163deebe627d404d6b264cffe6700cef
Sergioga06 <sergiogallardog06@gmail.com>
2024-11-22 12:14:58 +0100

    12:15

--- aemet/sggAemet.java
+++ aemet/sggAemet.java
@@ -2,8 +2,9 @@ package aemet;
 import aemet.sggRegCiudad;
 public class sggAemet {
     static final int NCIUDADES = 8; 
-    public static  String[] ciudades =
+    public static  String[] ciudades = {"Almeria", "Granada", "Cadiz", "Malaga", "Jaen", "Cordoba", "Huelva", "Sevilla" };
+
     public static void main(String[] args) {
-    
+        sggRegCiudad v1 = new sggRegCiudad()  ;
     }
 }
--- aemet/sggRegCiudad.java
+++ aemet/sggRegCiudad.java
@@ -2,46 +2,64 @@ package aemet;
 public class sggRegCiudad {
     final int SGGNDIAS = 7;
     final int SGGNTEMP = 3;
-    int sggtmax;
-    int sggtmin;
+    int sggtmax = 40;
+    int sggtmin = -1;
+    String sggciudad = "";
     String[] sggdias = {"Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"};
     int[][] sggtemperaturas = new int[SGGNDIAS][SGGNTEMP];
 
 
-    public sggciudad(int sggtmax, int sggtmin){
-        this.
+    public sggRegCiudad (String sggciudad, int sggtmax, int sggtmin){
+        this.sggciudad= sggciudad;
+        this.sggtmax = sggtmax;
+        this.sggtmin = sggtmin;
+
 
     }
+
     // public void sggsettmax(int sggtmax){
     //     this.sgg
     // }
 
-    public int sggtmax(){
+    // public int sggtmax(){
 
-    }
+    // }
 
-    public int [][] generarTemps(){
+    public int [][] sgggenerarTemps(){
         int temp;
         for (int i=0; i<SGGNDIAS; i++){
             for(int j=0; j<SGGNTEMP;j++){
-                temp =((int)(Math.random()*41)-1);
+                temp =((int)(Math.random()*41)-1);// numeros del -1 al 40
                 sggtemperaturas[i][j] = temp;
             }
         }
         return sggtemperaturas;
     }
-    public void imprimirDatos(){
+    public int sggtempMax(){
+        int max = 0;
+        for(int i=0; i<SGGNDIAS; i++){
+            for(int j=0; j<SGGNTEMP; j++){
+            if (sggtemperaturas[i][j]>max) {
+                max = sggtemperaturas[i][j];
+            }
+        }
+        }
+        return max;
+    }
+    public  
+    public void sggimprimirDatos(){
+
+        System.out.printf("%27s", " ");
         System.out.println("------ TEMPERATURAS ------");
-        System.out.printf("%20s", " ");
+        System.out.printf("%2s", " ");
         for (int i=0; i<SGGNDIAS; i++ ){
             System.out.printf("%10s", this.sggdias[i]);
         }
         System.out.println();
         System.out.println();
         for (int j=0; j<SGGNTEMP; j++){
-            // System.out.printf("%20s", this.sgg[j]);
             for (int i=0; i<SGGNDIAS;i++){
-                System.out.printf("%10.2f", this.sggtemperaturas[i][j]);
+                System.out.printf("%10.2s", this.sggtemperaturas[i][j]);
             }
             System.out.println();
             System.out.println();
